0bfb7c05e7409430ddd6af11b9cfef0c010ef9fc
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-16 17:31:53 +0530

    added post route

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {
         "message": "hello world"
+    }
+
+
+@app.get("/post")
+async def gets_post():
+    return {
+        "post": "this is your first post"
     }
