85bb2628a108fe8cf4cac8b8ac218aa4af6fe9dd
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-12-04 10:19:11 +0530

    add new post route

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
     return {
-        "message": "hello world"
+        "message": "hello Peter"
     }
 
 
@@ -15,3 +23,10 @@ async def gets_post():
     return {
         "post": "this is your first post"
     }
+
+#post request
+@app.post("/create_post")
+async def create_post(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
