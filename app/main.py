3fba8a8d6497a088f455159c2ab05fee2143ea9a
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-12-08 17:14:46 +0530

    create a post route

--- app/main.py
+++ app/main.py
@@ -59,22 +59,20 @@ async def gets_post():
 
 @app.get("/posts/{id}")
 async def get_post(id: int):
-    post = find_post(id)
-    print(post)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"detail": f"post with id: {id} was not found"}
+    cursor.execute("SELECT * FROM posts WHERE id = %s", (str(id)))
+    post = cursor.fetchone()
     return {"post_detail": post}
 
 
 # post request
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def create_post(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *"
+                   , (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    # commit the changes
+    conn.commit()
+    return {"data": new_post}
 
 
 # delete route
