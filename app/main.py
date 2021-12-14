1a5e60921e852d17186ff0ac4621acae2f9b5c06
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-12-14 10:39:35 +0530

    created a user authentication
    created a auth route for users

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import time
 from . import models, schemas, utils
 from sqlalchemy.orm import Session
 from .database import engine, get_db
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -43,6 +43,7 @@ def find_index_id(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
