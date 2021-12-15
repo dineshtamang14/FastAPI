f5b3f5186f0c4a79334958b0ef698904d8c8afad
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-12-15 10:40:43 +0530

    created verification function of jwt token

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,10 @@
-from jose import JWSError, jwt
+from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer("login")
 
 # secret key
 # ALGORITHM
@@ -19,3 +24,24 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        id: str = payload.get("users_id")
+
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail=f"could not validate credentials",
+                                          headers={"www-Authenticate": "Bearer"})
+    return verify_access_token(token, credentials_exception)
+
+
