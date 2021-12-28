62838bac612c4a4a581b893335625dfd8fc2f3cd
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-12-28 14:54:14 +0530

    create new revision

---
+++ alembic/versions/46e62a9580bb_add_user_table.py
@@ -0,0 +1,24 @@
+"""add user table
+
+Revision ID: 46e62a9580bb
+Revises: a2c37f9c5acd
+Create Date: 2021-12-28 14:28:31.897256
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '46e62a9580bb'
+down_revision = 'a2c37f9c5acd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ alembic/versions/d5f9d5bcc566_add_foreign_key_to_posts_table.py
@@ -0,0 +1,24 @@
+"""add foreign-key to posts table
+
+Revision ID: d5f9d5bcc566
+Revises: 46e62a9580bb
+Create Date: 2021-12-28 14:44:14.099714
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd5f9d5bcc566'
+down_revision = '46e62a9580bb'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
